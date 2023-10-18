7b22c67526835550a6d3cbcabce6de8d3d1218e9
osaigbovo idiaghe <idiagheosaigbovoosas@gmail.com>
2023-10-18 13:21:37 +0100

    add more features

--- day3/main.js
+++ day3/main.js
@@ -36,7 +36,7 @@ const questions = [
     question: "What goes up but never ever comes down?",
 
     answers: [
-      { text: "Price", correct: true },
+      { text: "Price", correct: false },
       { text: "Money", correct: false },
       { text: "Your age", correct: true },
       { text: "Words", correct: false },
@@ -59,6 +59,7 @@ function startQuiz() {
 }
 
 function showQuestion() {
+  resetState();
   let currentQuestion = questions[currentQuestionIndex];
   let questionNo = currentQuestionIndex + 1;
   questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
@@ -68,6 +69,62 @@ function showQuestion() {
     button.innerHTML = answers.text;
     button.classList.add("btn");
     answersButtons.appendChild(button);
+    if (answers.correct) {
+      button.dataset.correct = answers.correct;
+    }
+    button.addEventListener("click", checkAnswer);
   });
 }
+
+function resetState() {
+  nextButton.style.display = "none";
+  while (answersButtons.firstChild) {
+    answersButtons.removeChild(answersButtons.firstChild);
+  }
+}
+
+function checkAnswer(e) {
+  const selectBtn = e.target;
+  const isCorrect = selectBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectBtn.classList.add("correct");
+    score++;
+  } else {
+    selectBtn.classList.add("incorrect");
+  }
+  Array.from(answersButtons.children).forEach((button) => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+}
+
+function showScore() {
+  resetState();
+  questionElement.innerHTML =
+    "You got " + score + " out of " + questions.length + " questions correct!";
+  nextButton.innerHTML = "Play Again"
+  nextButton.addEventListener("click", startQuiz);
+  nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length){
+    showQuestion();
+  } else {
+    showScore();
+  }
+}
+
+
+nextButton.addEventListener("click", () => {
+  if (currentQuestionIndex < questions.length) {
+    handleNextButton();
+  } else {
+    startQuiz;
+  }
+});
 startQuiz();
